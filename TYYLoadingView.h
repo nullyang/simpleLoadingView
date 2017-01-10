1578ff78365b824459ec5e63abccdb2a05a2503e
nullyang <xingbie@yeah.net>
2017-01-10 14:12:00 +0800

    start repository

---
+++ TYYLoadingView.h
@@ -0,0 +1,17 @@
+//
+//  TYYViewController.m
+//  Pods
+//
+//  Created by Null on 17/1/6.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TYYLoadingView : UIView
+
++ (void)showLoadingView;
++ (void)showLoadingViewInView:(UIView *)view;
++ (void)hideLoadingView;
++ (void)hideLoadingViewInView:(UIView *)view;
+@end
